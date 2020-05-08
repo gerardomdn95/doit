62b66c455d619ee88127419e6591c445fce213c5
Gerardo Medina <gerardo@devf.mx>
2020-05-08 14:54:57 -0500

    add first structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Landing } from './views/Landing';
+import { Admin } from './views/Admin';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        {/* <Navbar /> */}
+        <Route exact path="/" component={Landing} key={0} />
+        <Route exact path="/admin" component={Admin} key={1} />
+      </>
+    </Router>
   );
 }
 

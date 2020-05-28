4881511a71fa68ee07fe8f5dc2e6bb6bbbe60d8b
Gerardo Medina <MY_NAME@example.com>
2020-05-27 23:17:27 -0500

    fix main image

--- src/components/MainLandingSection/MainLandingSection.js
+++ src/components/MainLandingSection/MainLandingSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import main from '../../assets/png/main.png';
+import main from '../../assets/jpg/main.jpg';
 import FadeIn from 'react-fade-in';
 import './mainLandingSection.scss'
 
@@ -8,19 +8,13 @@ const MainLandingSection = () => {
   return (
     <main className="main-container bg-dark">
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-6">
-            <FadeIn>
-              <div className="card">
-                <img className="img-fluid" src={main} alt="" />
-              </div>
-            </FadeIn>
-          </div>
-          <div className="col-12 col-md-6 d-flex flex-column justify-content-center">
-            <h1 className="text-light">About me</h1>
-            <p className="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, iste, odio deleniti eaque numquam molestias qui non quis natus provident a. Ipsa quidem fuga labore necessitatibus dolorum suscipit iure commodi!</p>
-            <p className="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi cupiditate labore dicta nemo voluptatem, repellendus assumenda, illum corrupti mollitia consequatur architecto qui quia, quam ea! Ullam quis repudiandae dolor distinctio!</p>
-          </div>
+        <div className="main-content">
+          <FadeIn>
+            <div className="card">
+              <img className="img-fluid" src={main} alt="" />
+            </div>
+            <p className="text-light">Artista mexicano ecléctico.</p>
+          </FadeIn>
         </div>
       </div>
     </main>
